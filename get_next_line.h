9f698bd53787a35d36c7c067f0b94e5eb8770936
Loic Pittet <lpittet@c2r5s2.42lausanne.ch>
2024-10-18 16:19:55 +0200

    header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 09:57:45 by lpittet           #+#    #+#             */
-/*   Updated: 2024/10/17 13:36:55 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/10/18 15:26:22 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
+#  define BUFFER_SIZE 4
 # endif
 
 //get_next_line.c
 char	*get_next_line(int fd);
 
-
 //get_next_line_utils.c
-
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char const *s2);
+int		find_new_line(char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strlen(const char *s);
 
 #endif
